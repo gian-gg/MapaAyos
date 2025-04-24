278dd998429696e057ddb517cca56b4c5bdc09d3
watsonjph <corsegajoseph@gmail.com>
2025-04-25 01:12:01 +0800

    CHORE: Improve basic security checks

--- models/UserModel.php
+++ models/UserModel.php
@@ -4,39 +4,57 @@ require_once __DIR__ . '/../config/db.php';
 function signUp($firstName, $lastName, $email, $hashedPassword) // a function for signing up a user
 {
     global $pdo;
-    $sql = "INSERT INTO users (firstName, lastName, email, password) 
+    try{
+        $sql = "INSERT INTO users (firstName, lastName, email, password) 
                 VALUES (:firstName, :lastName, :email, :password)";
 
-    $stmt = $pdo->prepare($sql);
+        $stmt = $pdo->prepare($sql);
 
-    $stmt->bindParam(':firstName', $firstName);
-    $stmt->bindParam(':lastName', $lastName);
-    $stmt->bindParam(':email', $email);
-    $stmt->bindParam(':password', $hashedPassword);
+        $stmt->bindParam(':firstName', $firstName);
+        $stmt->bindParam(':lastName', $lastName);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':password', $hashedPassword);
 
-    return $stmt->execute();
+        return $stmt->execute();
+    } catch (PDOException $e) {
+        error_log("Error signing up user: " . $e->getMessage());
+        return false; // can change this to throw a custom exception if needed
+    }
+    
 }
 
 function findUserByEmail($email) // a function to find a user by email
 {
     global $pdo;
-    $sql = "SELECT * FROM users WHERE email = :email LIMIT 1";
+    try {
+        $sql = "SELECT * FROM users WHERE email = :email LIMIT 1";
 
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(':email', $email);
-    $stmt->execute();
-
-    return $stmt->fetch(PDO::FETCH_ASSOC);
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+    
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        error_log("Error finding user by email: " . $e->getMessage());
+        return false;
+    }
+    
 }
 
 function findUserByID($userID) // a function to find a user by ID
 {
     global $pdo;
-    $sql = "SELECT * FROM users WHERE id = :id LIMIT 1";
-
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(':id', $userID);
-    $stmt->execute();
-
-    return $stmt->fetch(PDO::FETCH_ASSOC);
+    try {
+        $sql = "SELECT * FROM users WHERE id = :id LIMIT 1";
+    
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':id', $userID);
+        $stmt->execute();
+    
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        error_log("Error finding user by ID: " . $e->getMessage());
+        return false;
+    }
+    
 }
