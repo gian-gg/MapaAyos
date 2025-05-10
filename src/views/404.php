26893a30915dd87a268eb4bd416a8575832ce44a
Osbev Erica Cabucos <24104013@usc.edu.ph>
2025-05-10 11:53:12 +0800

    FEAT: enhance 404 page structure

--- src/views/404.php
+++ src/views/404.php
@@ -2,26 +2,35 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>MapaAyos - 404 Not Found</title>
 
-    <link rel="stylesheet" href="/MapaAyos/public/css/root.css">
-    <link rel="stylesheet" href="/MapaAyos/public/css/main.css">
-    <link rel="stylesheet" href="/MapaAyos/public/css/landing.css">
+    <link rel="stylesheet" href="/MapaAyos/public/css/root.css" />
+    <link rel="stylesheet" href="/MapaAyos/public/css/main.css" />
+    <link rel="stylesheet" href="/MapaAyos/public/css/landing.css" />
+    <link rel="stylesheet" href="/MapaAyos/public/css/404.css" />
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 </head>
 
 <body>
-    <div class="card">
-        <h1>MapaAyos</h1>
-        <div class="card-body">
-            <p>
-                <strong class="error">404</strong> - The page you are looking for does not exist.
-            </p>
-            <a href="/MapaAyos/" class="ma-btn">Home</a>
+    <div class="error-page-container">
+        <div class="error-background"></div>
+        <div class="error-content">
+            <div class="card-body">
+                <h1>Oops!</h1>
+                <p>
+                    <strong class="error">404 - PAGE NOT FOUND</strong><br>
+                    Oops! The page you’re looking for isn’t here. It might have been moved, deleted, or never existed. 
+                    Balik na lang tayo sa dashboard para ayos ulit ang lahat.
+                </p>
+                <a href="/MapaAyos/" class="ma-btn">
+                    <i class="bi bi-chevron-left"></i> | <p>Balik sa Dashboard</p> 
+                </a>
+            </div>
         </div>
     </div>
-
 </body>
 
 </html>
