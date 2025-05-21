2f219b76cf8f61a5d71dadc59427504b3814075c
spookyexe <gianepanto@gmail.com>
2025-05-22 03:17:49 +0800

    FIX: profile image header

--- src/views/components/header.php
+++ src/views/components/header.php
@@ -2,11 +2,11 @@
 
 function renderHeader($user)
 {
-    $profileImg = $user['hasProfilePic'] ? '/MapaAyos/public/uploads/pfp/' . $user['id'] . '.png' : '/MapaAyos/public/img/default-profile.png';
 
     echo "<div class='header'>";
     echo "<p>Welcome, " . ($user ? "{$user['firstName']} {$user['lastName']}" : "Guest") . "!</p>";
     if ($user) {
+        $profileImg = $user['hasProfilePic'] ? '/MapaAyos/public/uploads/pfp/' . $user['id'] . '.png' : '/MapaAyos/public/img/default-profile.png';
         echo "<img src='" . $profileImg . "' alt='Profile' class='header-profile-img'>";
     } else {
         echo "<div class='btn-group'><a style='display: flex; width: 99px; height: 32px; padding: 8px 16px; justify-content: center; align-items: center; gap: 5px; border-radius: 6px; border: 1px solid transparent; background: none; text-decoration: none; color: var(--text-primary); font-size: 14px; font-weight: 400; transition: all 0.2s ease;' onmouseover='this.style.textDecoration=\"underline\"' onmouseout='this.style.textDecoration=\"none\"' href='/signin'>Sign In</a>";
